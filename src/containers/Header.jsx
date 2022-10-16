99164a1566866a28ec74f857372b94c4dc110b28
Salman Khan <salmank4954@gmail.com>
2022-10-16 19:23:47 +0000

    initial commit

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='fixed top-0 w-[100%] border-b-2 py-4 shadow-sm bg-white'>
         <div className='container px-2 sm:px-0 flex items-center justify-between'>
           <Link to='/'>
-            <h1 className='text-[1.5rem] md:text-[2.5rem]'>Amazonis</h1>
+            <h1 className='text-[1.5rem] md:text-[2.5rem] font-bold'>Easy<span className='text-blue-500'>Buy</span></h1>
           </Link>
             <div className='flex items-center space-x-4'>
             <button className='border-2 px-5 py-2 rounded-lg border-black hover:text-white hover:bg-black transition-all'>Sign Up</button>
